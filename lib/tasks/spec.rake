9a88fea00c82e90a97e0b4c6b84940ef5c174811
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-05-11 12:58:03 +0300

    Add Rubocop to spec task execution flow, delete unneeded test

---
+++ lib/tasks/spec.rake
@@ -0,0 +1,13 @@
+require 'rubocop/rake_task'
+require 'rspec/core/rake_task'
+namespace :spec do
+  desc 'Execute rubocop -DR'
+  RuboCop::RakeTask.new(:rubocop) do |tsk|
+    tsk.options = ['-DR'] # Rails, display cop name
+    tsk.fail_on_error = false
+  end
+end
+Rake::Task[:spec].enhance do
+  Rake::Task['spec:rubocop'].invoke
+end
+
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -12,15 +12,4 @@
 require 'rails_helper'
 RSpec.describe Bid, :type => :model do
 
-  describe '#bid_creation_time' do
-
-    it 'bid_creation_time cannot be in the past,' do
-      mock_bid = Bid.new  bid_creation_time: DateTime.parse("2000-12-03T04:05:06+07:00 ")
-      mock_bid.valid?
-      expect(mock_bid.errors[:bid_creation_time]).to include("can't be in the past")
-    end
-
-
-  end
-
 end
