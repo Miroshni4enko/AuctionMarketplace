6533932b10c5cabb138db3b60e1edd6ecbc8a627
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-05-29 17:49:02 +0300

    Add lot_jid_closed and lot_jid_in_process for save appropriate jid of jobs, managing these fields

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -5,10 +5,15 @@
 # Table name: bids
 #
 #  id             :bigint(8)        not null, primary key
-#  user_id        :bigint(8)
-#  lot_id         :bigint(8)
-#  created_at     :datetime         not null
 #  proposed_price :float            not null
+#  created_at     :datetime         not null
+#  lot_id         :bigint(8)
+#  user_id        :bigint(8)
+#
+# Indexes
+#
+#  index_bids_on_lot_id   (lot_id)
+#  index_bids_on_user_id  (user_id)
 #
 
 require "rails_helper"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,28 +5,35 @@
 # Table name: users
 #
 #  id                     :bigint(8)        not null, primary key
-#  email                  :string           not null
-#  phone                  :string           not null
-#  first_name             :string           not null
-#  last_name              :string           not null
-#  birthday               :date             not null
-#  provider               :string           default("email"), not null
-#  uid                    :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
 #  allow_password_change  :boolean          default(FALSE)
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string
-#  last_sign_in_ip        :string
+#  birthday               :date             not null
+#  confirmation_sent_at   :datetime
 #  confirmation_token     :string
 #  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string           not null
+#  last_name              :string           not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  phone                  :string           not null
+#  provider               :string           default("email"), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  tokens                 :json
+#  uid                    :string           default(""), not null
+#  unconfirmed_email      :string
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
 
 require "rails_helper"
