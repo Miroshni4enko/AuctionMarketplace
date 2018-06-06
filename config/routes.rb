3c961722d46bc2784d4ee6e2f1ac9810f62f5d0e
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-06-06 15:15:37 +0300

    delete user testing controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   scope :api do
-    get "user/members_only", to: "user#members_only"
     mount_devise_token_auth_for "User", at: "auth", defaults: { format: :json }
 
 
--- spec/controllers/user_controller_specs/sessions_controller_spec.rb
+++ spec/controllers/user_controller_specs/sessions_controller_spec.rb
@@ -11,11 +11,7 @@ describe "Session user flow", type: :request do
     post "/api/auth/sign_in", params: { email: @user.email, password: @user.password }
     expect(response).to be_successful
     expect(controller.current_user).to eq(@user)
-
-    get user_members_only_path, headers: controller.headers
-    expect(response).to be_successful
     expect(response.header["access-token"]).to be
-    expect(controller.current_user).to eq(@user)
   end
 
   it "should sign out and delete access-token" do
